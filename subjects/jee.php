9645c46b260c336ffec84e895e60ee353a547ca3
Debarun Dey <141635359+debarundey@users.noreply.github.com>
2025-05-13 15:39:33 +0530

    Add files via upload

---
+++ subjects/jee.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<link rel="shortcut icon" type="png" href="../images/icon/logo.png">
+	<title> Sample Question Papers</title>
+	<link rel="stylesheet" type="text/css" href="sample_paper.css">
+	<script type="text/javascript" src="../script.js"></script>
+</head>
+
+<body>
+	<!-- Navigation Bar -->
+	<header>
+        <div class="logo">
+            <a href="../index.php">
+                <img src="../images/icon/brand.png" alt="logo">
+            </a>
+        </div>
+        <h1>Sample Papers 2018-23</h1>
+        <nav>
+            <ul>
+                <li><a href="../index.php">Home</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+	<div class="content-title">
+		<p>Explore a journey of knowledge in our sample paper, delving into diverse topics and fostering intellectual curiosity and understanding.</p>
+	</div>
+	<div class="sample">
+		<ul>
+			<li><a href="../samplePapers/p1.pdf" target="_blank">Sample Paper 2018 Set-A</a></li>
+			<li><a href="../samplePapers/p1.pdf" target="_blank">Sample Paper 2018 Set-B</a></li>
+			<li><a href="../samplePapers/p1.pdf" target="_blank">Sample Paper 2018 Set-C</a></li>
+		</ul>
+		<ul>
+			<li><a href="../samplePapers/p1.pdf" target="_blank">Sample Paper 2019 Set-A</a></li>
+			<li><a href="../samplePapers/p1.pdf" target="_blank">Sample Paper 2019 Set-B</a></li>
+			<li><a href="../samplePapers/p1.pdf" target="_blank">Sample Paper 2019 Set-C</a></li>
+		</ul>
+		<ul>
+			<li><a href="../samplePapers/p1.pdf" target="_blank">Sample Paper 2020 Set-A</a></li>
+			<li><a href="../samplePapers/p1.pdf" target="_blank">Sample Paper 2020 Set-B</a></li>
+			<li><a href="../samplePapers/p1.pdf" target="_blank">Sample Paper 2020 Set-C</a></li>
+		</ul>
+	</div>
+	<div class="sample lastSample">
+		<ul>
+			<li><a href="../samplePapers/p1.pdf" target="_blank">Sample Paper 2021 Set-A</a></li>
+			<li><a href="../samplePapers/p1.pdf" target="_blank">Sample Paper 2021 Set-B</a></li>
+			<li><a href="../samplePapers/p1.pdf" target="_blank">Sample Paper 2021 Set-C</a></li>
+		</ul>
+		<ul>
+			<li><a href="../samplePapers/p1.pdf" target="_blank">Sample Paper 2022 Set-A</a></li>
+			<li><a href="../samplePapers/p1.pdf" target="_blank">Sample Paper 2022 Set-B</a></li>
+			<li><a href="../samplePapers/p1.pdf" target="_blank">Sample Paper 2022 Set-C</a></li>
+		</ul>
+		<ul>
+			<li><a href="../samplePapers/p1.pdf" target="_blank">Sample Paper 2023 Set-A</a></li>
+			<li><a href="../samplePapers/p1.pdf" target="_blank">Sample Paper 2023 Set-B</a></li>
+			<li><a href="../samplePapers/p1.pdf" target="_blank">Sample Paper 2023 Set-C</a></li>
+		</ul>
+	</div>
+	<!-- FOOTER -->
+	<footer>
+		<h2>Contact Us</h2><br>
+		<p align="justify">If you have any questions or need assistance, feel free to reach out:</p>
+		<p><img src="../images/icon/telephone.png"><span> +91-33-4602XXXX</span><br><img src="../images/icon/email.png"><span>&nbsp;
+				educore2024@gmail.com</span></p>
+		<div class="social-media">
+			<a href="#" target="_blank"><img src="../images/icon/facebook (1).png"></a>
+			<a href="#" target="_blank"><img src="../images/icon/instagram (2).png"></a>
+			<a href="#" target="_blank"><img src="../images/icon/youtube.png"></a>
+			<a href="#" target="_blank"><img src="../images/icon/twitter.png"></a>
+		</div>
+	</footer>
+
+</body>
+
+</html>
